b09322a34a29f7566d7d2434f26e56b3f6c43106
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-09 17:42:17 +0200

    initiation python

---
+++ main.py
@@ -0,0 +1,26 @@
+# nom = input("Quel est votre nom?")
+# age = input("Quel est votre age?")
+
+
+nom = "Hamza"
+age = 32
+    
+
+
+# fonction str = pouvoir mélanger des class, ici string + int
+
+#print("Je suis " + nom + (str)age + " ans!")
+
+
+
+age_prochain = age + 1
+
+# nouvelle variable déclarée permettant de récupérer
+# la valeur de la variable et d'y ajouter 1
+
+
+
+print("L'an prochain vous aurez " +str(age_prochain) + " ans!")
+
+# integration de la nouvelle variable dans le print pour ajouter 1
+
