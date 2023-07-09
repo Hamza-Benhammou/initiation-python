838117a8da1946164a235876e294071013689c26
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-09 17:52:44 +0200

    beginner python

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ age_prochain = age + 1
 # nouvelle variable déclarée permettant de récupérer
 # la valeur de la variable et d'y ajouter 1
 
+
 print("L'an prochain vous aurez " +str(age_prochain) + " ans!")
 
 # integration de la nouvelle variable dans le print pour ajouter 1
