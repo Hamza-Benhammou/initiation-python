85c70dbb31026b7fc82b98e959ffa689a3c95dc5
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-11 16:05:55 +0200

    create a string value

---
+++ string.py
@@ -0,0 +1,10 @@
+# When you create a new variable, you can add a value called string, you have to put " at the beginning and another " 
+# at the end of your value, for example:
+
+# string = string of characters
+
+name = "John Doe"
+
+car = "BMW"
+
+day = "Monday"
