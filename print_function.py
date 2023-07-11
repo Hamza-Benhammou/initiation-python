47486e4284ec454664658d38281af8c84466b957
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-11 15:43:39 +0200

    how to print a value

---
+++ print_function.py
@@ -0,0 +1,5 @@
+# after creating a new variable called price with value, and calculate half price, you have to print the  new value like this:
+
+price = 240
+half_price = price / 2
+print(half_price)
