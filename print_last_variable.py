0e5d5b06fe8fa09cd8f9e52d3eddce527bfcab28
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-11 16:23:03 +0200

    function print can print only the last variable

---
+++ print_last_variable.py
@@ -0,0 +1,8 @@
+# When you create variables with the same name, you can print the result of the variable but only the value of the last variable 
+# will be displayed in the console, for example:
+
+status = "watching tv"
+status = "eating burger"
+print(status)
+
+# when you will print only the value of the last variable will be displayed, in that case : eating burger
