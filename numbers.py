f6298c0485ebf85985153586091db450d10194d5
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-12 16:14:09 +0200

    numbers in python

---
+++ numbers.py
@@ -0,0 +1,13 @@
+# if you want to create a variable with numbers, don't use double quotes, you can just write it like this:
+
+number_of_cars = 10
+
+# you can also calculate with + or - like this:
+
+number_of_cars = 10 + 2
+people = 12 - 3
+
+# or * and / like this:
+
+number_of_cars = 10 * 2
+people = 12 / 3
