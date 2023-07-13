790e27ece4ee90b4c9704f56cdf0eb053535500e
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-13 17:28:02 +0200

    convert degree radian

---
+++ degree_radians_converter.py
@@ -0,0 +1,13 @@
+# how to convert angle degree to radian:
+# if 1° = π/180
+
+angle_deg = 90
+angle_rad = angle_deg * (3.14 / 180)
+print(angle_rad)
+
+# how to convert angle radian to degree:
+
+
+angle_rad = 1.57
+angle_deg = angle_rad * (180 / 3.14)
+print(angle_deg)
--- numbers.py
+++ numbers.py
@@ -4,10 +4,10 @@ number_of_cars = 10
 
 # you can also calculate with + or - like this:
 
-number_of_cars = 10 + 2
+number_of_bikes = 10 + 2
 people = 12 - 3
 
 # or * and / like this:
 
-number_of_cars = 10 * 2
-people = 12 / 3
+number_of_bus = 10 * 2
+kids = 12 / 3
