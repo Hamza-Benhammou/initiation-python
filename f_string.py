dca4f0c6b0dbcd7ec4c109ec30a80a24e7af7990
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-14 18:00:15 +0200

    how to print several differents class values

---
+++ f_string.py
@@ -0,0 +1,8 @@
+# f_string allows to print several different class values:
+
+name = "John"
+age = 30
+# we have a first class str and the second class int
+print(f"My name is {name} I'm {age} years old")
+
+# use f and double quotes to print different class values, you must write the name of your variables in braces
