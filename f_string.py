c1a94fe0a6d00648bdd4b4d4432e309b7bdebba9
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-14 18:04:28 +0200

    print differents class values

--- f_string.py
+++ f_string.py
@@ -3,6 +3,7 @@
 name = "John"
 age = 30
 # we have a first class str and the second class int
-print(f"My name is {name} I'm {age} years old")
+texte = f"My name is {name} I'm {age} years old"
+print(texte)
 
 # use f and double quotes to print different class values, you must write the name of your variables in braces
