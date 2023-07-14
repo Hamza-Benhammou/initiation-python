e320e359f28861b545a30ef3815602b897eeb2a1
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-14 15:07:03 +0200

    how to print number of characters

---
+++ display_number_of_characters.py
@@ -0,0 +1,9 @@
+# if you want to display the number of characters:
+# step 1 create a variables
+
+name = "Dustin Poirier"
+print(name)
+print(len(name))
+
+# use len to print the number of characters in the string
+
