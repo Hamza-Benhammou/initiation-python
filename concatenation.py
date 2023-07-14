56d22caff244602b21fc6f64ea710dbf39fada6f
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-14 15:14:05 +0200

    concatenation

---
+++ concatenation.py
@@ -0,0 +1,10 @@
+# concatenation is used to combine multiple values, for example:
+
+name = "Khabib"
+surname = "Nurmagomedov"
+full_name = name + " " + surname
+print(full_name)
+
+# use + to combine multiple values
+# now you can directly print the last variable to display all previous values
+# double quotes with space are only here to generate space between name and surname
