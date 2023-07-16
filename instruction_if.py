9d52c1496de7950491ed97031497b1122ce1a70d
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-16 20:37:42 +0200

    how to use instruction if

---
+++ instruction_if.py
@@ -0,0 +1,9 @@
+# use the if statement for a condition:
+
+username = "John"
+if True:
+    print("Hello " , username)
+
+
+# if your condition is True, the code will run:
+# if your condtion is False, the code will not run:
