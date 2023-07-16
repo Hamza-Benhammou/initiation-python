3d48c655ed8f15494ec20fd7f80ffb0748ef5159
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-16 21:03:24 +0200

    how to use for loop

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+# use the for loop to use all elements of a list
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in my_list:
+    print(i, i*i)
+
+# here calculated the square of each digit using the for loop to use all items in the list
+
+
