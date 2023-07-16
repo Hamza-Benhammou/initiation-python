9659ab275017f483507b428e0da6cd5c3da0200c
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-07-16 20:44:33 +0200

    how to use instruction else

---
+++ instruction_else.py
@@ -0,0 +1,12 @@
+# if your condition is not correct you can use else for another statement
+
+username = "Daniel"
+
+if username == "Daniel":
+    print("Hello, " + username)
+else:
+    print("Sorry you are not allowed")
+
+# here the condition is true, so the first instruction will be executed
+# if it were false, the console would display the message that is in the else statement
+
